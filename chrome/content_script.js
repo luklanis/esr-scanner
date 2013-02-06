8dc5c6895da7378511c4686ccc87938d5444c0f5
Lukas Landis <lukas.landis@kaba.com>
2013-02-06 11:55:25 +0100

    remove old tries

--- chrome/content_script.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if (request.coderow) {
-        console.log(request.coderow);
-        document.activeElement.value = request.coderow;
-        sendResponse({result: "set"});
-    }
-});
